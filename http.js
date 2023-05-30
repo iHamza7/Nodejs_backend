25db62fe2fbff9ef5169d936de3466bb45af72c1
iHamza7 <khanazam3324@gmail.com>
2023-05-30 22:08:06 +0500

    changed

---
+++ http.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+//file
+
+//server
+const servere = http.createServer((req, res) => {
+  res.end("hello from server");
+});
+
+servere.listen(8000, "127.0.0.1", () => {
+  console.log("listening to port ");
+});
